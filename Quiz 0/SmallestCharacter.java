8e98a843a1b22330965f2ad21c45de88e21a5597
Mert ERGÜN <mertergun17@hotmail.com>
2024-03-09 13:33:52 +0300

    Solved Smallest Character quiz

--- Quiz 0/SmallestCharacter.java
+++ Quiz 0/SmallestCharacter.java
@@ -16,5 +16,56 @@ public class SmallestCharacter {
         // [1]
         // If there are more than one integer to print then:
         // [1,2,3,4,5]
+
+        File file = new File(args[0]);
+
+        // Input file format is structured as below:
+        // Line 0: query-1 query-2 query-3 ...
+        // Line 1: word-1 word-2 word-3 ...
+
+        //Let the function f(s) be the frequency of the lexicographically smallest character
+        //in a non-empty string s. For example, if s = "dcce" then f(s) = 2 because the
+        //lexicographically smallest character is ’c’, which has a frequency of 2.
+        //You are given an array words of string words and another array queries of string
+        //queries. For each query queries[i], count the number of words in words such that
+        //f(queries[i]) < f(W) for each W in words.
+        //Return an integer array answer, where each answer[i] is the answer to the ith query.
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String[] queries = scanner.nextLine().split(" ");
+            String[] words = scanner.nextLine().split(" ");
+            scanner.close();
+
+            int[] answer = new int[queries.length];
+
+            for (int i = 0; i < queries.length; i++) {
+                int count = 0;
+                for (String word : words) {
+                    if (f(queries[i]) < f(word)) {
+                        count++;
+                    }
+                }
+                answer[i] = count;
+            }
+
+            System.out.println(Arrays.toString(answer));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+       
+    }
+
+    public static int f(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                return freq[i];
+            }
+        }
+        return 0;
     }
 }
