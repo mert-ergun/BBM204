094bdb7c7807218c8d7ed217f5c640e7cd19cddd
Mert ERGÜN <mertergun17@hotmail.com>
2024-04-06 19:38:05 +0300

    Assignment 2 Initial Commit

---
+++ Assignment 2/src/Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Main class
+ */
+// FREE CODE HERE
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+       /** MISSION POWER GRID OPTIMIZATION BELOW **/
+
+        System.out.println("##MISSION POWER GRID OPTIMIZATION##");
+        // TODO: Your code goes here
+        // You are expected to read the file given as the first command-line argument to read 
+        // the energy demands arriving per hour. Then, use this data to instantiate a 
+        // PowerGridOptimization object. You need to call getOptimalPowerGridSolutionDP() method
+        // of your PowerGridOptimization object to get the solution, and finally print it to STDOUT.
+        System.out.println("##MISSION POWER GRID OPTIMIZATION COMPLETED##");
+
+        /** MISSION ECO-MAINTENANCE BELOW **/
+
+        System.out.println("##MISSION ECO-MAINTENANCE##");
+        // TODO: Your code goes here
+        // You are expected to read the file given as the second command-line argument to read
+        // the number of available ESVs, the capacity of each available ESV, and the energy requirements 
+        // of the maintenance tasks. Then, use this data to instantiate an OptimalESVDeploymentGP object.
+        // You need to call getMinNumESVsToDeploy(int maxNumberOfAvailableESVs, int maxESVCapacity) method
+        // of your OptimalESVDeploymentGP object to get the solution, and finally print it to STDOUT.
+        System.out.println("##MISSION ECO-MAINTENANCE COMPLETED##");
+    }
+}
---
+++ Assignment 2/src/PowerGridOptimization.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * This class accomplishes Mission POWER GRID OPTIMIZATION
+ */
+public class PowerGridOptimization {
+    private ArrayList<Integer> amountOfEnergyDemandsArrivingPerHour;
+
+    public PowerGridOptimization(ArrayList<Integer> amountOfEnergyDemandsArrivingPerHour){
+        this.amountOfEnergyDemandsArrivingPerHour = amountOfEnergyDemandsArrivingPerHour;
+    }
+
+    public ArrayList<Integer> getAmountOfEnergyDemandsArrivingPerHour() {
+        return amountOfEnergyDemandsArrivingPerHour;
+    }
+    /**
+     *     Function to implement the given dynamic programming algorithm
+     *     SOL(0) <- 0
+     *     HOURS(0) <- [ ]
+     *     For{j <- 1...N}
+     *         SOL(j) <- max_{0<=i<j} [ (SOL(i) + min[ E(j), P(j − i) ] ]
+     *         HOURS(j) <- [HOURS(i), j]
+     *     EndFor
+     *
+     * @return OptimalPowerGridSolution
+     */
+    public OptimalPowerGridSolution getOptimalPowerGridSolutionDP(){
+        // TODO: YOUR CODE HERE
+        return null;
+    }
+}
