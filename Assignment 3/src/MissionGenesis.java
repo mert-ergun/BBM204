78b8b16ebbc88788060f20f8c1773f82fe4aa110
Mert ERGÜN <mertergun17@hotmail.com>
2024-04-18 17:50:49 +0300

    Implemented the first part of the Assignment

--- Assignment 3/src/MissionGenesis.java
+++ Assignment 3/src/MissionGenesis.java
@@ -1,4 +1,16 @@
 // Class representing the mission of Genesis
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document; // Import the Document class
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File; // Import the File class
+import java.util.ArrayList;
+import java.util.List;
+
 public class MissionGenesis {
 
     // Private fields
@@ -18,8 +30,57 @@ public class MissionGenesis {
     // Method to read XML data from the specified filename
     // This method should populate molecularDataHuman and molecularDataVitales fields once called
     public void readXML(String filename) {
+        try {
+        // Parse the XML file
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(filename));
 
-        /* YOUR CODE HERE */ 
-        
+         // Get Human and Vitales data nodes
+         NodeList humanDataNodes = document.getElementsByTagName("HumanMolecularData");
+         NodeList vitalesDataNodes = document.getElementsByTagName("VitalesMolecularData");
+ 
+         // Extract and store molecule data for humans
+         molecularDataHuman = extractMolecularData(humanDataNodes);
+ 
+         // Extract and store molecule data for Vitales
+         molecularDataVitales = extractMolecularData(vitalesDataNodes);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Helper function to extract molecule data from NodeList
+    private MolecularData extractMolecularData(NodeList dataNodes) {
+        List<Molecule> molecules = new ArrayList<>();
+
+        // Iterate over each molecule node
+        for (int i = 0; i < dataNodes.getLength(); i++) {
+            Node moleculeNode = dataNodes.item(i).getFirstChild();
+            while (moleculeNode != null) {
+                if (moleculeNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element moleculeElement = (Element) moleculeNode;
+
+                    // Extract molecule information
+                    String id = moleculeElement.getElementsByTagName("ID").item(0).getTextContent();
+                    int bondStrength = Integer.parseInt(moleculeElement.getElementsByTagName("BondStrength").item(0).getTextContent());
+
+                    // Extract bonded molecule IDs
+                    List<String> bondedMoleculeIds = new ArrayList<>();
+                    NodeList bondNodes = moleculeElement.getElementsByTagName("MoleculeID");
+                    for (int j = 0; j < bondNodes.getLength(); j++) {
+                        bondedMoleculeIds.add(bondNodes.item(j).getTextContent());
+                    }
+
+                    // Create Molecule object and add to list
+                    molecules.add(new Molecule(id, bondStrength, bondedMoleculeIds));
+                }
+                moleculeNode = moleculeNode.getNextSibling();
+            }
+        }
+
+        return new MolecularData(molecules);
     }
 }
--- Assignment 3/src/MolecularData.java
+++ Assignment 3/src/MolecularData.java
@@ -19,18 +19,79 @@ public class MolecularData {
     // Method to identify molecular structures
     // Return the list of different molecular structures identified from the input data
     public List<MolecularStructure> identifyMolecularStructures() {
-        ArrayList<MolecularStructure> structures = new ArrayList<>();
-
-        /* YOUR CODE HERE */ 
-
+        List<MolecularStructure> structures = new ArrayList<>();
+        Set<String> visited = new HashSet<>(); // Keep track of visited molecules
+    
+        for (Molecule molecule : molecules) {
+            if (!visited.contains(molecule.getId())) {
+                dfs(molecule, structures, visited); // Perform DFS to build the structure
+            }
+        }
+    
         return structures;
     }
+    
+
+    // Helper function for depth-first search to build molecular structures
+    private void dfs(Molecule molecule, List<MolecularStructure> structures, Set<String> visited) {
+        visited.add(molecule.getId());
+    
+        // Check if any existing structure contains a molecule bonded to the current one
+        MolecularStructure existingStructure = findStructureWithBondedMolecule(molecule, structures);
+    
+        if (existingStructure != null) {
+            // Merge current molecule and its connected components into the existing structure
+            existingStructure.addMolecule(molecule);
+            for (String bondedId : molecule.getBonds()) {
+                Molecule bondedMolecule = findMoleculeById(bondedId);
+                if (bondedMolecule != null && !visited.contains(bondedId)) {
+                    dfs(bondedMolecule, structures, visited);
+                }
+            }
+        } else {
+            // Create a new structure for the current molecule and its connected components
+            MolecularStructure newStructure = new MolecularStructure();
+            newStructure.addMolecule(molecule);
+            structures.add(newStructure);
+            for (String bondedId : molecule.getBonds()) {
+                Molecule bondedMolecule = findMoleculeById(bondedId);
+                if (bondedMolecule != null && !visited.contains(bondedId)) {
+                    dfs(bondedMolecule, structures, visited);
+                }
+            }
+        }
+    }
+
+    private MolecularStructure findStructureWithBondedMolecule(Molecule molecule, List<MolecularStructure> structures) {
+        for (MolecularStructure structure : structures) {
+            for (String bondedId : molecule.getBonds()) {
+                if (structure.hasMolecule(bondedId)) {
+                    return structure;
+                }
+            }
+        }
+        return null;
+    }
+
+    // Helper function to find a molecule by its ID
+    private Molecule findMoleculeById(String id) {
+        for (Molecule molecule : molecules) {
+            if (molecule.getId().equals(id)) {
+                return molecule;
+            }
+        }
+        return null;
+    }    
 
     // Method to print given molecular structures
     public void printMolecularStructures(List<MolecularStructure> molecularStructures, String species) {
-        
-        /* YOUR CODE HERE */ 
-
+        System.out.println(molecularStructures.size() + " molecular structures have been discovered in " + species + ".");
+        int structureCount = 1;
+        for (MolecularStructure structure : molecularStructures) {
+            System.out.print("Molecules in Molecular Structure " + structureCount + ": ");
+            System.out.println(structure.toString());
+            structureCount++; 
+        }
     }
 
     // Method to identify anomalies given a source and target molecular structure
@@ -38,7 +99,18 @@ public class MolecularData {
     public static ArrayList<MolecularStructure> getVitalesAnomaly(List<MolecularStructure> sourceStructures, List<MolecularStructure> targeStructures) {
         ArrayList<MolecularStructure> anomalyList = new ArrayList<>();
         
-        /* YOUR CODE HERE */ 
+        for (MolecularStructure targetStructure : targeStructures) {
+            boolean isAnomaly = true;
+            for (MolecularStructure sourceStructure : sourceStructures) {
+                if (targetStructure.equals(sourceStructure)) {
+                    isAnomaly = false;
+                    break;
+                }
+            }
+            if (isAnomaly) {
+                anomalyList.add(targetStructure);
+            }
+        }
 
         return anomalyList;
     }
@@ -46,7 +118,11 @@ public class MolecularData {
     // Method to print Vitales anomalies
     public void printVitalesAnomaly(List<MolecularStructure> molecularStructures) {
 
-        /* YOUR CODE HERE */ 
+        System.out.println("Molecular structures unique to Vitales individuals:");
+        for (MolecularStructure structure : molecularStructures) {
+            System.out.println(structure.toString());
+        }
+
 
     }
 }
